b58e81ff9a3f06d41a1fb69bf50a968dd5724ce8
Alejandro Jorge <alejandro.jorge@vipera.com>
2020-11-12 11:42:16 +0100

    Implemented help and echo commands

--- WriteItDownApplication.py
+++ WriteItDownApplication.py
@@ -12,6 +12,9 @@ def getMessages():
     messagesDict = json.loads(messageJson)
     return messagesDict
 
+def sendMessage(idchat, text):
+    requests.get(configuration.url + "sendMessage?text=" + text + "&chat_id=" + str(idchat))
+
 def save(update, context):
     itemList = ItemsList(context.args[0], context.args[1], context.args[2])
     items.append(itemList)
@@ -19,15 +22,41 @@ def save(update, context):
     update.message.reply_text(itemList.showList())
 
 def echo(update, context):
-    update.message.reply_text(update.message.text)
+    print(context.args)
+    if not len(context.args) == 0 or context.args == None: update.message.reply_text(" ".join(update.message.text.split("/echo")))
+    else: update.message.reply_text("Don't be shy, send anything! 😛")
 
-def sendMessage(idchat, text):
-    requests.get(configuration.url + "sendMessage?text=" + text + "&chat_id=" + str(idchat))
+def help(update, context):
+    allCommands = f"List of commands: 😎\n\n"
+    allCommands += "1. /echo text: Return the sent message.\n"
+    allCommands += "2. /save name items hour : Save a list and set a remainder hour.\n"
+    allCommands += "3. /add name items : Add items to an existing list.\n"
+    allCommands += "4. /remove name : Delete an existing list.\n"
+    allCommands += "5. /show name : Show a description's list.\n"
+    allCommands += "6. /changeName oldName newtName : Change the name of a created list.\n"
+    allCommands += "7. /changeHour name hour : Change the remainder hour of a created list. If the new hour is = 0, the remainder hour will be deleted.\n"
+    allCommands += "8. /removeItems name items : Delete the items of a list.\n"
+    allCommands += "9. /changeItems oldItems . newItems : Remove old items and add the new items.\n"
+    update.message.reply_text(allCommands)
+
+def add(update, context):
+    pass
+
+def remove(update, context):
+    pass
+
+def show(update, context):
+    pass
 
 def main():
     updater = Updater(configuration.token, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("save", save))
+    dp.add_handler(CommandHandler("echo", echo))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("add", add))
+    dp.add_handler(CommandHandler("remove", remove))
+    dp.add_handler(CommandHandler("show", show))
     dp.add_handler(MessageHandler(Filters.text, echo))
     updater.start_polling()
     updater.idle()
