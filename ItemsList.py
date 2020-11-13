522a40f9a8ba4358223a5932952d07dc24199351
Alejandro Jorge <alejandro.jorge@vipera.com>
2020-11-13 13:02:06 +0100

    Method remove inplemented. WIP error checks.

--- ItemsList.py
+++ ItemsList.py
@@ -2,10 +2,10 @@ class ItemsList:
 
     def __init__(self, name, items, hour):
         self.name = name
-        self.items = items.split(" ")
+        self.items = " ".join(items)
         self.hour = hour
     
     def showList(self):
-        itemsInLines = lambda items: '\n'.join(items)
-        itemsToShow = itemsInLines(self.items)
-        return f"List '{self.name}'\n--------------\nItems: {itemsToShow}\nHour: {self.hour}"
+        #itemsInLines = lambda items: '\n'.join(items)
+        #itemsToShow = itemsInLines(self.items)
+        return f"List '{self.name}'\n--------------\nItems: {self.items}\nHour: {self.hour}"
--- WriteItDownApplication.py
+++ WriteItDownApplication.py
@@ -5,32 +5,48 @@ from ItemsList import ItemsList
 from BotConfiguration import BotConfiguration
 from MongoDbConfiguration import MongoDbConfiguration
 from DictToObject import DictToObject
+import logging
 
-items = []
+log = logging.getLogger(__name__)
 configuration = BotConfiguration()
 mongo = MongoDbConfiguration()
 
 def save(update, context):
-    itemList = ItemsList(context.args[0], context.args[1], context.args[2])
-    document = mongo.db.itemsList.find({"_id": update.message.chat.id})
-    try:
-        if len(list(document)) == 0:  
-            mongo.db.itemsList.insert_one({"_id": update.message.chat.id, "lists": [itemList.__dict__]})
-        else:  
-            mongo.db.itemsList.update_one({"_id": update.message.chat.id}, {"$push": {"lists": itemList.__dict__ }})
-        message = "Saved list!\n\n" + itemList.showList()
-        update.message.reply_text(message)
-    except StopIteration as err:
-        print("StopIteration error:", err, "-- rewinding Cursor object.")
-        document.rewind()
+    logMethod("/save", context)
+    if(len(context.args) < 3): 
+        invalidCommandMessage(update)
+    else:
+        totalArgs = len(context.args)
+        itemList = ItemsList(context.args[0], context.args[1:totalArgs-1], context.args[totalArgs - 1])
+        document = mongo.db.itemsList.find({"_id": update.message.chat.id})
+        try:
+            if len(list(document)) == 0:
+                document.rewind()  
+                mongo.db.itemsList.insert_one({"_id": update.message.chat.id, "lists": [itemList.__dict__]})
+            else:  
+                mongo.db.itemsList.update_one({"_id": update.message.chat.id}, {"$push": {"lists": itemList.__dict__ }})
+            message = "Saved list!\n\n" + itemList.showList()
+            update.message.reply_text(message)
+        except StopIteration as err:
+            print("StopIteration error:", err, "-- rewinding Cursor object.")
+            document.rewind()
 
 def echo(update, context):
-    if not context.args == None or len(context.args) == 0: update.message.reply_text(" ".join(update.message.text.split("/echo")))
-    else: update.message.reply_text("Don't be shy, send anything! 😛")
+    logMethod("/echo", context)
+    update.message.reply_text("Welcome to WriteItDown Bot! 😈")
+    help(update, context)
+
+def repeat(update, context):
+    logMethod("/repeat", context)
+    if not (context.args == None or len(context.args) == 0): 
+        update.message.reply_text(" ".join(update.message.text.split("/repeat")))
+    else: 
+        update.message.reply_text("Don't be shy, send anything! 😛")
 
 def help(update, context):
+    logMethod("/help", context)
     allCommands = f"List of commands: 😎\n\n"
-    allCommands += "1. /echo text: Returns the sent message.\n"
+    allCommands += "1. /repeat text: Returns the sent message.\n"
     allCommands += "2. /save name items hour : Saves a list and set a remainder hour.\n"
     allCommands += "3. /add name items : Adds items to an existing list.\n"
     allCommands += "4. /remove name : Deletes an existing list.\n"
@@ -43,42 +59,64 @@ def help(update, context):
     update.message.reply_text(allCommands)
 
 def add(update, context):
+    logMethod("/add", context)
     pass
 
 def remove(update, context):
-    pass
+    logMethod("/remove", context)
+    if not len(context.args) == 1: invalidCommandMessage(update)
+    else:
+        #document = mongo.db.itemsList.find({"_id": update.message.chat.id, "lists": [{"name": context.args[0]}]})
+        mongo.db.itemsList.update_one({"_id": update.message.chat.id},  { "$pull" : {"lists": { "name" : context.args[0]}}})
+        update.message.reply_text("Done!")
 
 def show(update, context):
-    try:
-        #document = mongo.db.itemsList.find({"_id": update.message.chat.id, "lists": [{"name": context.args[0]}]})
-        document = mongo.db.itemsList.find({"_id": update.message.chat.id})
-        message = "Does not exist a list with this name!"
-        for value in document.next().get("lists"):
-            if value["name"] == context.args[0]:
-                message = ItemsList(value["name"], " ".join(value["items"]), value["hour"]).showList()
-                break
-        update.message.reply_text(message)
-    except StopIteration as err:
-        print("StopIteration error:", err, "-- rewinding Cursor object.")
-        document.rewind()
+    logMethod("/show", context)
+    if not len(context.args) == 1: invalidCommandMessage(update)
+    else:
+        try:
+            #document = mongo.db.itemsList.find({"_id": update.message.chat.id, "lists": [{"name": context.args[0]}]})
+            document = mongo.db.itemsList.find({"_id": update.message.chat.id})
+            message = ""
+            if len(list(document)) > 0:
+                document.rewind()
+                for value in document.next().get("lists"):
+                    if value["name"] == context.args[0]:
+                        message = ItemsList(value["name"], [value["items"]], value["hour"]).showList()
+                        break
+                update.message.reply_text(message)
+            else: update.message.reply_text("Does not exist a list with this name!")
+        except StopIteration as err:
+            print("StopIteration error:", err, "-- rewinding Cursor object.")
+            document.rewind()
 
 def showAll(update, context):
+    logMethod("/showAll", context)
+    if not len(context.args) == 0: invalidCommandMessage(update)
     try:
         document = mongo.db.itemsList.find({"_id": update.message.chat.id})
         message = ""
-        for value in document.next().get("lists"):
-                message += ItemsList(value["name"], " ".join(value["items"]), value["hour"]).showList() + '\n'
-        if message == "": update.message.reply_text("You don't have lists for now, create one!")
-        else: update.message.reply_text(message)
+        if len(list(document)) > 0:
+            document.rewind()
+            for value in document.next().get("lists"):
+                    message += ItemsList(value["name"], [value["items"]], value["hour"]).showList() + '\n'
+            update.message.reply_text(message)
+        else: update.message.reply_text("You don't have lists for now, create one!")
     except StopIteration as err:
         print("StopIteration error:", err, "-- rewinding Cursor object.")
         document.rewind()
+
+def invalidCommandMessage(update):
+    update.message.reply_text("Incorrect commands!")
+
+def logMethod(method, context):
+    log.info(f"Method {method} {context.args}")
 
 def main():
     updater = Updater(configuration.token, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("save", save))
-    dp.add_handler(CommandHandler("echo", echo))
+    dp.add_handler(CommandHandler("repeat", repeat))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("add", add))
     dp.add_handler(CommandHandler("remove", remove))
